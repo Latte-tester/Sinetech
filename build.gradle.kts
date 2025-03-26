c95edb8936f50608ce929b537a095c254332a638
GitLatte <portisroad@gmail.com>
2025-03-26 04:33:35 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "GitLatte/Sinetech")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/GitLatte/Sinetech")
     }
 
     android {
