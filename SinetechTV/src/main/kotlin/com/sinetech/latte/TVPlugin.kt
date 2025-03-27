466eadfc141a8ade824ad4a325fb2e1823c87854
GitLatte <portisroad@gmail.com>
2025-03-27 07:27:44 +0300

    Update TVPlugin.kt

--- SinetechTV/src/main/kotlin/com/sinetech/latte/TVPlugin.kt
+++ SinetechTV/src/main/kotlin/com/sinetech/latte/TVPlugin.kt
@@ -11,9 +11,9 @@ class TVPlugin : Plugin() {
     private val sharedPref = activity?.getSharedPreferences("SinetechTV", Context.MODE_PRIVATE)
     private val playlistsToLang = mapOf(
         "playlist_iptvsevenler.m3u8" to "tr",
-        "playlist_power-yabanci-dizi.m3u8" to "tr",
-        "playlist_someone.m3u8" to "tr",
-        "playlist_power-sinema.m3u8" to "tr"
+        "playlist_power-yabanci-dizi.m3u8" to "en",
+        "playlist_someone.m3u8" to "de",
+        "playlist_power-sinema.m3u8" to "fr"
     )
 
     init {
