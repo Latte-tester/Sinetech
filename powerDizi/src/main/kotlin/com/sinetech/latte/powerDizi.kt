29750a4dab078e9a4ee21cfe44fac9ca480026ef
GitLatte <portisroad@gmail.com>
2025-03-28 08:50:25 +0300

    Update powerDizi.kt

--- powerDizi/src/main/kotlin/com/sinetech/latte/powerDizi.kt
+++ powerDizi/src/main/kotlin/com/sinetech/latte/powerDizi.kt
@@ -207,7 +207,11 @@ class powerDizi(private val sharedPref: SharedPreferences?) : MainAPI() {
         }
         
         // Notify the app to refresh the main page
-        HomeViewModel.updateHomePageEvent.invoke()
+        MainActivity.activity?.apply {
+            runOnUiThread {
+                MainActivity.activity?.recreate()
+            }
+        }
         Log.d("IPTV", "loadData » $loadData")
 
         val kanallar = IptvPlaylistParser().parseM3U(app.get(mainUrl).text)
@@ -353,60 +357,20 @@ class IptvPlaylistParser {
         return Playlist(playlistItems)
     }
 
-    /** Replace "" (quotes) from given string. */
     private fun String.replaceQuotesAndTrim(): String {
         return replace("\"", "").trim()
     }
 
-    /** Check if given content is valid M3U8 playlist. */
     private fun String.isExtendedM3u(): Boolean = startsWith(EXT_M3U)
 
-    /**
-     * Get title of media.
-     *
-     * Example:-
-     *
-     * Input:
-     * ```
-     * #EXTINF:-1 tvg-id="1234" group-title="Kids" tvg-logo="url/to/logo", Title
-     * ```
-     *
-     * Result: Title
-     */
     private fun String.getTitle(): String? {
         return split(",").lastOrNull()?.replaceQuotesAndTrim()
     }
 
-    /**
-     * Get media url.
-     *
-     * Example:-
-     *
-     * Input:
-     * ```
-     * https://example.com/sample.m3u8|user-agent="Custom"
-     * ```
-     *
-     * Result: https://example.com/sample.m3u8
-     */
     private fun String.getUrl(): String? {
         return split("|").firstOrNull()?.replaceQuotesAndTrim()
     }
 
-    /**
-     * Get url parameter with key.
-     *
-     * Example:-
-     *
-     * Input:
-     * ```
-     * http://192.54.104.122:8080/d/abcdef/video.mp4|User-Agent=Mozilla&Referer=CustomReferrer
-     * ```
-     *
-     * If given key is `user-agent`, then
-     *
-     * Result: Mozilla
-     */
     private fun String.getUrlParameter(key: String): String? {
         val urlRegex     = Regex("^(.*)\\|", RegexOption.IGNORE_CASE)
         val keyRegex     = Regex("$key=(\\w[^&]*)", RegexOption.IGNORE_CASE)
@@ -415,25 +379,6 @@ class IptvPlaylistParser {
         return keyRegex.find(paramsString)?.groups?.get(1)?.value
     }
 
-    /**
-     * Get attributes from `#EXTINF` tag as Map<String, String>.
-     *
-     * Example:-
-     *
-     * Input:
-     * ```
-     * #EXTINF:-1 tvg-id="1234" group-title="Kids" tvg-logo="url/to/logo", Title
-     * ```
-     *
-     * Result will be equivalent to kotlin map:
-     * ```Kotlin
-     * mapOf(
-     *   "tvg-id" to "1234",
-     *   "group-title" to "Kids",
-     *   "tvg-logo" to "url/to/logo"
-     * )
-     * ```
-     */
     private fun String.getAttributes(): Map<String, String> {
         val extInfRegex      = Regex("(#EXTINF:.?[0-9]+)", RegexOption.IGNORE_CASE)
         val attributesString = replace(extInfRegex, "").replaceQuotesAndTrim().split(",").first()
@@ -447,29 +392,16 @@ class IptvPlaylistParser {
             .toMap()
             .toMutableMap()
 
-        // Set default values for missing attributes
         if (!attributes.containsKey("tvg-country")) {
-            attributes["tvg-country"] = "TR"
+            attributes["tvg-country"] = "TR/Altyazılı"
         }
         if (!attributes.containsKey("tvg-language")) {
-            attributes["tvg-language"] = "TR;EN"
+            attributes["tvg-language"] = "TR/Altyazılı"
         }
 
         return attributes
     }
 
-    /**
-     * Get value from a tag.
-     *
-     * Example:-
-     *
-     * Input:
-     * ```
-     * #EXTVLCOPT:http-referrer=http://example.com/
-     * ```
-     *
-     * Result: http://example.com/
-     */
     private fun String.getTagValue(key: String): String? {
         val keyRegex = Regex("$key=(.*)", RegexOption.IGNORE_CASE)
 
@@ -483,9 +415,7 @@ class IptvPlaylistParser {
     }
 }
 
-/** Exception thrown when an error occurs while parsing playlist. */
 sealed class PlaylistParserException(message: String) : Exception(message) {
 
-    /** Exception thrown if given file content is not valid. */
     class InvalidHeader : PlaylistParserException("Invalid file header. Header doesn't start with #EXTM3U")
 }
