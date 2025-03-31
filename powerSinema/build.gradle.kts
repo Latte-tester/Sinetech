269f60be4b0a9f7ce1a9477ef847809e2c9f77ba
GitLatte <portisroad@gmail.com>
2025-03-31 03:55:15 +0300

    Update build.gradle.kts

--- powerSinema/build.gradle.kts
+++ powerSinema/build.gradle.kts
@@ -4,10 +4,10 @@ android {
     buildFeatures {
         buildConfig = true
     }
-}
-
-buildConfig {
-    buildConfigField("String", "TMDB_SECRET_API", "\"3b0b95d3f6c1c2f3c8f3c3f3c8f3c3f3\"")
+    
+    defaultConfig {
+        buildConfigField("String", "TMDB_SECRET_API", "\"3b0b95d3f6c1c2f3c8f3c3f3c8f3c3f3\"")
+    }
 }
 
 cloudstream {
