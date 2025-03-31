b5a4612ec33d178a5d372c5c76585031054f323c
GitLatte <portisroad@gmail.com>
2025-03-31 10:03:31 +0300

    icon fix.

--- powerDizi/build.gradle.kts
+++ powerDizi/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("TvSeries")
-    iconUrl = "https://www.google.com/s2/favicons?domain=tr.canlitv.team&sz=%size%"
+    iconUrl = "https://raw.githubusercontent.com/GitLatte/Sinetech/main/powerDizi/src/main/resources/favicon.svg"
 }
