8d849ce79c528053d2fc4b9484ceff99bc3a44d8
GitLatte <portisroad@gmail.com>
2025-04-05 06:07:37 +0300

    ..

--- KoolTO/build.gradle.kts
+++ KoolTO/build.gradle.kts
@@ -1,14 +1,4 @@
-version = 4
-
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-}
-
-android {
-    buildFeatures {
-        buildConfig = true
-    }
-}
+version = 1
 
 cloudstream {
     authors     = listOf("GitLatte", "patr0nq")
