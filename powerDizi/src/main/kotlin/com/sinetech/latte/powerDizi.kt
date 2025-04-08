7f5e49663238f931a8799e7d9e751e8ae57883f9
Latte <portisroad@gmail.com>
2025-04-08 21:51:39 +0300

    Update powerDizi.kt

--- powerDizi/src/main/kotlin/com/sinetech/latte/powerDizi.kt
+++ powerDizi/src/main/kotlin/com/sinetech/latte/powerDizi.kt
@@ -224,7 +224,7 @@ class powerDizi(private val sharedPref: SharedPreferences?) : MainAPI() {
                 val match = episodeRegex.find(title)
                 if (match != null) {
                     val (_, season, episode) = match.destructured
-                    Episode(
+                    newEpisode(
                         episode = episode.toInt(),
                         season = season.toInt(),
                         data = LoadData(
