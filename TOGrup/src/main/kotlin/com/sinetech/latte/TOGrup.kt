5a21a2374f86b90dacf24a47e6967d540b178024
GitLatte <portisroad@gmail.com>
2025-04-17 01:13:54 +0300

    Update TOGrup.kt

--- TOGrup/src/main/kotlin/com/sinetech/latte/TOGrup.kt
+++ TOGrup/src/main/kotlin/com/sinetech/latte/TOGrup.kt
@@ -57,7 +57,7 @@ class TOGrup : MainAPI() {
                     }
                 }
 
-                HomePageList(title, show, isHorizontalImages = true)
+                HomePageList(groupTitle, show, isHorizontalImages = true)
             },
             hasNext = false
         )
